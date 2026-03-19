0f14c12e77931e28dbbe5969f6533d0134c49b0b
Sariel Har-Peled <sariel@red.sarielhp.org>
2026-03-19 08:31:03 +0100

    Time/date: 03_19_26__08_31_03 from SarielLaptop

---
+++ scripts/src/deps.jl
@@ -0,0 +1,11 @@
+#! /bin/env julial
+
+import Pkg;
+
+Pkg.activate( "." )
+Pkg.add( "DelimitedFiles" )
+Pkg.add( "CSV" )
+Pkg.add( "DataFrames" )
+Pkg.add( "LsqFit" )
+Pkg.add( "Plots" )
+Pkg.add( "Printf" )
